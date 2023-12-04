5c69895cc6a5f76bf4745852641db7cb96bcd1a6
Jorge Jaramillo <48229449+Jaramilion@users.noreply.github.com>
2023-12-04 15:12:27 -0700

    Revert "Initial commit"
    This reverts commit 49e853f561f37d6261ce131efd706b38ffdf9d34.

--- api/src/models/index.ts
+++
@@ -1,9 +0,0 @@
-import {Todos, TodosSchema } from './todos.model'
-import {Sequelize} from 'sequelize'
-
-// @ts-ignore
-function setupModels(sequelize) {
-    Todos.init(TodosSchema, Todos.config(sequelize))
-}
-
-module.exports = setupModels;
